69a42ce550ef094f86f118be3ea7e60470b2b012
danielista <61905525+danielista@users.noreply.github.com>
2021-03-09 10:11:12 +0100

    fšetko funguje zatiaľ ;)

---
+++ src/main/java/org/example/Controller.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/hello")
+    public String getHello(){
+        return "Hello HOW'S GOING? ";
+    }
+
+    @RequestMapping("/time")
+    public String getTime(){
+        String pattern = "EEEEE dd MMMMM yyyy HH:mm:ss";
+        SimpleDateFormat simpleDateFormat =
+                new SimpleDateFormat(pattern, new Locale("en", "US"));
+
+        String date = simpleDateFormat.format(new Date());
+        return date;
+        //return new Date().toString();
+    }
+
+    @RequestMapping("/myfirstpage")
+    public String getMorning(){
+        return "<h1>Good Morning</h1> " +
+                "<p style=\"color:red;\"> ... </p>";
+    }
+
+    @RequestMapping("/myfirstpage/{username}")
+    public String getMorning2(@PathVariable String username){
+        return "<h1 style=\"text-align:center;\" >Good Morning</h1> " +
+                "<p style=\"text-align:center;\"> " +username+" </p>";
+    }
+
+}
